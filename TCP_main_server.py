ed0e256add9056d1e1cd7853dbb748716bfdc95b
MahatKC <mkarvat@gmail.com>
2022-08-04 13:00:58 -0300

    server

--- TCP_main_server.py
+++ TCP_main_server.py
@@ -1,9 +1,9 @@
-from TCP_server import TCP_server 
+from TCP_server import TCP_server_AES, TCP_server_RSA
 import time
 
-HEADER_SIZE = 66
+HEADER_SIZE = 54
 
 for i in range(12):
-    TCP_server(HEADER_SIZE, (i==9))
+    TCP_server_AES(HEADER_SIZE, (i==11))
     print(f"Finished iteration {i}")
     time.sleep(0.1)
--- TCP_server.py
+++ TCP_server.py
@@ -1,52 +1,90 @@
 import socket
 import time
-import pickle
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP, AES
+from Crypto.Random import get_random_bytes
 
 HOST = "123.123.123.123"  
 PORT = 55443  
 
-def TCP_server(HEADER_SIZE, extra_sleep):
+def TCP_server_RSA(extra_sleep):
     f = open('arquivo_recebido.txt', 'wb')
 
     #generate the RSA key
-    RSAKey = RSA.generate(1024)
+    rsa_key = RSA.generate(1024)
+    rsa_private_key = rsa_key.export_key()
     
-    RSAPubKey = RSAKey.publickey()
+    rsa_public_key = rsa_key.publickey().export_key()
 
-    print(RSAPubKey)
-    print(type(RSAPubKey))
+    file_out = open("public_key.pem", "wb")
+    file_out.write(rsa_public_key)
+    file_out.close()
+
+    file_out = open("private.pem", "wb")
+    file_out.write(rsa_private_key)
+    file_out.close()
+
+    private_key = RSA.import_key(open("private.pem").read())
+    chiper_rsa = PKCS1_OAEP.new(private_key)
     
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         
         conn, addr = s.accept()
-        #l = conn.recv(1000 - HEADER_SIZE)
-        pickle_dump = pickle.dumps(RSAPubKey)
-        print(pickle_dump)
-        print(len(pickle_dump))
-        conn.send(pickle.dumps(RSAPubKey))
 
-        rcstring = ''
-        while 1:
-            buf = conn.recv(1024)
-            rcstring += buf
-            if not len(buf):
-                break
+        file_out = open("public_key.pem", "rb")
+        l = file_out.read(271)
+        conn.send(l)
+        file_out.close()
 
-        """
-        while(l):
+        encrypted_l = conn.recv(1024)
+
+        while(encrypted_l):
+            for i in range(8):
+                chunk = encrypted_l[128*i:128*(i+1)]
+                if i==0:
+                    l = chiper_rsa.decrypt(chunk)
+                else:
+                    l += chiper_rsa.decrypt(chunk)
             f.write(l)
-            l = conn.recv(1000 - HEADER_SIZE)
+            encrypted_l = conn.recv(1024)
         if extra_sleep:
             time.sleep(5)
-        """
+        
         f.close()
         conn.close()
 
-        encmessage = pickle.loads(rcstring)
-        print(encmessage)
+def TCP_server_AES(HEADER_SIZE, extra_sleep):
+    f = open('arquivo_recebido.txt', 'wb')
+    key = get_random_bytes(16)
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        
+        conn, addr = s.accept()
 
-if __name__ == '__main__':
-    TCP_server(66, False)
+        conn.send(key)
+
+        encrypted_l = conn.recv(1000-HEADER_SIZE)
+        nonce = encrypted_l[:16]
+        tag = encrypted_l[16:32]
+        chunk = encrypted_l[32:]
+        ciphertext = chunk
+        cipher = AES.new(key, AES.MODE_EAX, nonce)
+
+        while(chunk):
+            chunk = conn.recv(1000-HEADER_SIZE)
+            ciphertext += chunk
+        if extra_sleep:
+            time.sleep(5)
+        
+        t0=time.time()
+        l = cipher.decrypt_and_verify(ciphertext, tag)
+        f.write(l)
+        t1=time.time()
+        print(f"tempo: {t1-t0}")
+        
+        f.close()
+        conn.close()
